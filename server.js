0314a5d802db87405a5bf4b59a3aef50231f7c85
BrunoForcato <bruno_hebeler@hotmail.com>
2020-05-11 16:40:53 +0000

    Alterei o texto

--- server.js
+++ server.js
@@ -3,7 +3,20 @@ const express = require('express');
 const server = express();
 
 server.get('/', function(request, response) {
+    response.send('Lista Garagems');
+})
+
+server.post('/', function(request, response) {
     response.send('Hello World!');
 })
+
+server.put('/', function(request, response) {
+    response.send('Hello World!');
+})
+
+server.delete('/', function(request, response) {
+    response.send('Hello World!');
+})
+
 
 server.listen(process.env.PORT || 3000);
